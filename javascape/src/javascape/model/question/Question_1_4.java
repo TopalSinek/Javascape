0916301e435f21f1b9c0f40249191c985a901c3c
cgdsyilmaz <cagdasyilmaz96@gmail.com>
2015-05-11 20:46:31 +0300

    3 sorun var
    1. Compilerda array döndüren metodlarda sıkıntı var, error falan değil
    ama code not working diyo, testerları ve metodun kodlarını kontrol ettim
    (cevapları txt olarak koydum) bi sorun yok sanırım onlarda.
    
    2.Number of tries utku konuştuk kafasına göre takılıyo.
    
    3.Can bittikten sonra save/load fileı için yer istiyo bu güzel ama oyun
    aynı yerde kalıyo ve oyun kapanmıyo kapanmalı. Bi de çıkan error msgde
    bi typo var "You got co(a)ught." yazıyo

--- javascape/src/javascape/model/question/Question_1_4.java
+++ javascape/src/javascape/model/question/Question_1_4.java
@@ -1,5 +1,7 @@
 package javascape.model.question;
 
+import java.lang.reflect.Method;
+
 import javascape.model.Question;
 
 public class Question_1_4 extends Question {
@@ -8,26 +10,26 @@ public class Question_1_4 extends Question {
 	}
 
 	public boolean test(Class myClass) throws Exception {
-//		Method withoutDoublesMethod = myClass.getMethod("withoutDoubles",
-//				int.class, int.class, boolean.class);
-//
-//		// test method
-//		Object resB = withoutDoublesMethod.invoke(myClass.newInstance(), 2, 3,
-//				true);
-//		int wdI1 = ((Integer) resB).intValue();
-//
-//		resB = withoutDoublesMethod.invoke(myClass.newInstance(), 3, 3, true);
-//		int wdI2 = ((Integer) resB).intValue();
-//
-//		resB = withoutDoublesMethod.invoke(myClass.newInstance(), 6, 6, true);
-//		int wdI3 = ((Integer) resB).intValue();
-//
-//		resB = withoutDoublesMethod.invoke(myClass.newInstance(), 6, 6, false);
-//		int wdI4 = ((Integer) resB).intValue();
-//
-//		if (wdI1 == 5 && wdI2 == 6 && wdI3 == 7 && wdI4 == 12)
+		Method withoutDoublesMethod = myClass.getMethod("withoutDoubles",
+				int.class, int.class, boolean.class);
+
+		// test method
+		Object resB = withoutDoublesMethod.invoke(myClass.newInstance(), 2, 3,
+				true);
+		int wdI1 = ((Integer) resB).intValue();
+
+		resB = withoutDoublesMethod.invoke(myClass.newInstance(), 3, 3, true);
+		int wdI2 = ((Integer) resB).intValue();
+
+		resB = withoutDoublesMethod.invoke(myClass.newInstance(), 6, 6, true);
+		int wdI3 = ((Integer) resB).intValue();
+
+		resB = withoutDoublesMethod.invoke(myClass.newInstance(), 6, 6, false);
+		int wdI4 = ((Integer) resB).intValue();
+
+		if (wdI1 == 5 && wdI2 == 7 && wdI3 == 7 && wdI4 == 12)
 			return true;
-//		return false;
+		return false;
 	}
 
 	public String getTextInside() {
--- javascape/src/javascape/model/question/Question_2_1.java
+++ javascape/src/javascape/model/question/Question_2_1.java
@@ -1,5 +1,7 @@
 package javascape.model.question;
 
+import java.lang.reflect.Method;
+
 import javascape.model.Question;
 
 public class Question_2_1 extends Question {
@@ -9,29 +11,29 @@ public class Question_2_1 extends Question {
 
 	public boolean test(Class myClass) throws Exception {
 
-//		Method squareUpMethod = myClass.getMethod("squareUp", int.class);
-//
-//		// test method
-//		Object resB = squareUpMethod.invoke(myClass.newInstance(), 0);
-//		int[] sqI1 = (int[]) (resB);
-//
-//		resB = squareUpMethod.invoke(myClass.newInstance(), 1);
-//		int[] sqI2 = (int[]) (resB);
-//
-//		resB = squareUpMethod.invoke(myClass.newInstance(), 4);
-//		int[] sqI3 = (int[]) (resB);
-//		
-//		resB = squareUpMethod.invoke(myClass.newInstance(), 3);
-//		int[] sqI4 = (int[]) (resB);
-//
-//		int[] res1 = {};
-//		int[] res2 = { 1 };
-//		int[] res3 = { 0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1 };
-//		int[] res4 = { 0, 0, 1, 0, 2, 1, 3, 2, 1 };
-//
-//		if (sqI1 == res1 && sqI2 == res2 && sqI3 == res3 && sqI4 == res4)
+		Method squareUpMethod = myClass.getMethod("squareUp", int.class);
+
+		// test method
+		Object resB = squareUpMethod.invoke(myClass.newInstance(), 0);
+		int[] sqI1 = (int[]) (resB);
+
+		resB = squareUpMethod.invoke(myClass.newInstance(), 1);
+		int[] sqI2 = (int[]) (resB);
+
+		resB = squareUpMethod.invoke(myClass.newInstance(), 4);
+		int[] sqI3 = (int[]) (resB);
+		
+		resB = squareUpMethod.invoke(myClass.newInstance(), 3);
+		int[] sqI4 = (int[]) (resB);
+
+		int[] res1 = {};
+		int[] res2 = { 1 };
+		int[] res3 = { 0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1 };
+		int[] res4 = { 0, 0, 1, 0, 2, 1, 3, 2, 1 };
+
+		if (sqI1 == res1 && sqI2 == res2 && sqI3 == res3 && sqI4 == res4)
 			return true;
-//		return false;
+		return false;
 	}
 
 	public String getTextInside() {
